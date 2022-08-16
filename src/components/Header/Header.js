6e45068d572ab318989014401e5e8473b32c30f1
Prashant Pal <prashantpal2468@gmail.com>
2022-08-16 14:06:52 +0530

    changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import style from "./Header.module.css";
 const Header = () => {
   return (
     <nav>
-        <h1 className={style.temph1}/>
+        <h1 className={style.temph1}>&nbsp</h1>
         <h1>Create New Account</h1>
         <h2>Conatct us</h2>
     </nav>
