c977e6187df5e9238234796004e8ce5c624b98b6
Prashant Pal <prashantpal2468@gmail.com>
2022-08-19 17:36:33 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import Header from "./components/Header/Header";
-import Content from "./components/Content/Content";
+// import Header from "./components/Header/Header";
+// import Content from "./components/Content/Content";
+import Content from "./components/form_components/Content/Content";
 import style from "./App.css";
+import Header from "./components/form_components/Header/Header";
 function app(){
   return(
     <div className={style.body}>
